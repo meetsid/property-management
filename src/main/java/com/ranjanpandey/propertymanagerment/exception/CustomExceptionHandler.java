92fa5c9f92ea63f18f29a49c59278483ba1ee309
Siddhesh <meetsid007@yahoo.co.in>
2025-02-02 10:01:51 -0500

    slf4j changes added.

--- src/main/java/com/ranjanpandey/propertymanagerment/exception/CustomExceptionHandler.java
+++ src/main/java/com/ranjanpandey/propertymanagerment/exception/CustomExceptionHandler.java
@@ -1,19 +1,22 @@
 package com.ranjanpandey.propertymanagerment.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import java.awt.geom.RectangularShape;
 import java.util.ArrayList;
 import java.util.List;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
-
+    //slf4j logger added
+    //Both below lines are valid CustomExceptionHandler.class is equivalent to this.getClass()
+    //private final Logger logger = LoggerFactory.getLogger(CustomExceptionHandler.class);
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<List<ErrorModel>> handleFieldValdiation(MethodArgumentNotValidException manv){
         List<FieldError> fieldErrorsList = manv.getBindingResult().getFieldErrors();
@@ -21,6 +24,8 @@ public class CustomExceptionHandler {
         ErrorModel errorModel;
 
         for(FieldError fe: fieldErrorsList){
+            logger.debug("Inside field validation {} - {}",fe.getField(),fe.getDefaultMessage());
+            logger.info("Inside field validation {} - {}",fe.getField(),fe.getDefaultMessage());
             errorModel = new ErrorModel();
             errorModel.setCode(fe.getField());
             errorModel.setMessaage(fe.getDefaultMessage());
@@ -32,9 +37,14 @@ public class CustomExceptionHandler {
 
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<List<ErrorModel>>  handleBusinessException(BusinessException businessException){
-        System.out.println("Business Exception is thrown");
+        for(ErrorModel em: businessException.getErrors()){
+            //System.out.println("Business Exception is thrown");
+            logger.debug("Inside field validation {} - {}",em.getCode(),em.getMessaage());
+            logger.info("Inside field validation {} - {}",em.getCode(),em.getMessaage());
+            logger.warn("Inside field validation {} - {}",em.getCode(),em.getMessaage());
+            logger.error("Inside field validation {} - {}",em.getCode(),em.getMessaage());
+        }
+
         return new ResponseEntity<>(businessException.getErrors(), HttpStatus.BAD_REQUEST);
     }
-
-
 }
